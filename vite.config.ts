64390ee064aecc5811e38a9dd341135a631dc4fb
rajsam55 <149546644+rajsam55@users.noreply.github.com>
2026-06-26 21:57:37 +0530

    Fix syntax for defining process.env.GEMINI_API_KEY

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(() => {
     },
     define : {
       
-      `process.env.GEMINI_API_KEY`  : JSON.stringify(env.GEMINI_API_KEY)
+      process.env.GEMINI_API_KEY  : JSON.stringify(env.GEMINI_API_KEY)
 
 
       
